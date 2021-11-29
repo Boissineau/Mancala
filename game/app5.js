e75ced38b11a0917a7fd81b740bad3bf5364b070
IamMarkT <markt1084@yahoo.com>
2021-11-29 16:14:49 -0500

    moved old app6

--- game/app5.js
+++ game/app5.js
@@ -2,19 +2,14 @@ const m4 = twgl.m4;
 const v3 = twgl.v3;
 let time = 0;
 
-// let objURL = [
-//     "./assets/board/new/mancala.obj",
-//     "./assets/raymanModel.obj",
-// ];
 
-// https://stackoverflow.com/questions/3798848/most-efficient-way-to-draw-multiple-identical-objects
-// https://stackoverflow.com/questions/3846359/efficient-way-of-drawing-in-opengl-es
-// https://webglfundamentals.org/webgl/lessons/webgl-drawing-multiple-things.html
+let boardURL = [
+    "./assets/board/new/mancala2.obj",
+    // "./assets/pebble_OBJ/pebble2.obj"
+];
 
-let objURL = [
+let beanURL = [
     "./assets/pebble_OBJ/pebble2.obj"
-        // "./assets/board/new/mancala2.obj",
-
 ];
 
 import { Models } from "./assets/models.js";
@@ -29,12 +24,15 @@ if (!gl) {
 }
 
 let sceneProgram = sceneProgramInfo(gl);
-// let boxProgram = boxProgramInfo(gl);
+let beanProgram = beanProgramInfo(gl);
 
-let models = new Models();
+
+let board = new Models();
+let bean = new Models();
+
 
 let near = 0.1;
-let far = 2.5;
+let far = 2;
 let radius = 1;
 let canvasWidth = gl.canvas.width;
 let canvasHeight = gl.canvas.height;
@@ -42,22 +40,67 @@ let aspect = canvasWidth / canvasHeight;
 let modelDim = undefined;
 let cameraLookAt = undefined;
 let modelObj = undefined;
+let beanObj = undefined;
+
 let y_angle = 0;
 // let x_angle = document.getElementById("myRange").value
-let fov_Y = 75;
+let fov_Y = 90;
 
-let slider = document.getElementById("myRange");
 let x_angle = undefined;
 
+let drag = false;
+let click = false;
+let mouseDownX = 1;
+let mouseDownY = 1;
+
+// canvas.add... for clicking on canvas only
+document.addEventListener("mousedown", (event) => {
+    click = true;
+    mouseDownX = event.clientX;
+    mouseDownY = event.clientY;
+});
+
+let mouseUpX = 1;
+let mouseUpY = 1;
+document.addEventListener("mousemove", (event) => {
+    if (!click) return;
+    drag = true;
+    mouseUpX = event.clientX;
+    mouseUpY = event.clientY;
+});
+
+document.addEventListener("mouseup", () => {
+    click = false;
+    if (drag) {
+        drag = false;
+    }
+});
+
+// zoom in and out
+let zoom = -8;
+document.addEventListener("wheel", (event) => {
+    let scale = event.deltaY * -0.01;
+    if (zoom + scale >= -3) scale = 0;
+    if (zoom + scale <= -20) scale = 0;
+    zoom += scale;
+});
+
+let modelMatrix = m4.identity();
+let xRot = m4.identity();
+let yRot = m4.identity();
+let identityMatrix = m4.identity();
+let angle = { x: 0, y: 0 };
 async function main() {
-    await models.getModelData(objURL);
-    modelDim = models.modelExtents[0];
+    await board.getModelData(boardURL);
+    await bean.getModelData(beanURL);
+
+
+    modelDim = board.modelExtents[0];
     cameraLookAt = modelDim.center;
-    modelObj = models.vertexAttributes[1];
+    modelObj = board.vertexAttributes[0];
+    beanObj = bean.vertexAttributes[0];
 
     let render = () => {
-        x_angle = slider.value;
-        
         gl.enable(gl.DEPTH_TEST);
         gl.clearColor(0.3, 0.4, 0.5, 1);
         gl.enable(gl.CULL_FACE);
@@ -66,12 +109,42 @@ async function main() {
         gl.viewport(0, 0, canvasWidth, canvasHeight);
         gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
+
+        if (drag) {
+            angle = getAngle(mouseDownX, mouseDownY, mouseUpX, mouseUpY);
+            mat4.rotate(xRot, identityMatrix, angle.x / 4, [0, 1, 0]);
+            // finds y rotation
+            mat4.rotate(yRot, identityMatrix, angle.y / 16, [1, 0, 0]);
+            //multiplies them together to create the new look
+            mat4.mul(modelMatrix, xRot, yRot);
+        } else {
+            // let angleSeconds = performance.now() / 1000 / 6 * 2 * Math.PI;
+            // angle.x = angleSeconds
+            // angle.y = angleSeconds / 4
+            // mat4.rotate(xRot, identityMatrix, angle.x, [0, 1, 0]);
+            // // finds y rotation
+            // mat4.rotate(yRot, identityMatrix, angle.y, [1, 0, 0]);
+            // //multiplies them together to create the new look
+            // mat4.mul(modelMatrix, xRot, yRot);
+        }
+
+
         renderScene(
             sceneProgram,
-            getViewMatrix(radius, deg2rad(x_angle), deg2rad(y_angle)),
+            getViewMatrix(radius, deg2rad(angle.x), deg2rad(angle.y)),
             getProjectionMatrix(fov_Y, near, far),
             modelObj
         );
+
+        renderBeans(
+            beanProgram,
+            getViewMatrix(radius, deg2rad(angle.x), deg2rad(angle.y)),
+            beanObj
+        );
+
+
+
+
         requestAnimationFrame(render);
     };
     requestAnimationFrame(render);
@@ -103,31 +176,11 @@ function getProjectionMatrix(fov, near, far) {
     );
 }
 
-// function getCameraTransformationMatrix (scale) {
-//     const s = modelDim.dia / 8;
-//     const rotationTransformation = m4.multiply(
-//       m4.rotationY(deg2rad(y_angle)),
-//       m4.rotationX(deg2rad(x_angle))
-//     );
-//     const translationVector = v3.add(
-//       cameraLookAt,
-//       v3.mulScalar(
-//         m4.transformDirection(rotationTransformation, [0, 0, 1]),
-//         radius * modelDim.dia
-//       )
-//     );
-
-//     return m4.multiply(
-//       m4.multiply(m4.translation(translationVector), rotationTransformation),
-//       scale ? m4.scaling([s, s, s]) : m4.identity()
-//     );
-// }
-
 function renderScene(programInfo, viewMatrix, projectionMatrix, model) {
     gl.useProgram(programInfo.program);
     const uniforms = {
         n2c: 0,
-        modelMatrix: m4.identity(),
+        modelMatrix: modelMatrix,
         viewMatrix,
         projectionMatrix,
     };
@@ -139,123 +192,61 @@ function renderScene(programInfo, viewMatrix, projectionMatrix, model) {
     });
 }
 
-// function renderCamera() {
-//     const viewMatrix =  getViewMatrix(7.5, 0, 0, cameraLookAt, modelDim),
-//     projectionMatrix =  getProjectionMatrix(25, 0.1, 10, aspect, modelDim)
+function renderBeans(programInfo, viewMatrix, model) {
 
-//     const uniforms = {
-//       n2c: 1,
-//       modelMatrix: m4.identity(),
-//       viewMatrix,
-//       projectionMatrix
-//     };
+    // Scaling beans
+    // let beanSX = 0.00125, beanSY = 0.00125, beanSZ = 0.00125;
+    let beanSX = 0.1, beanSY = 0.1, beanSZ = 0.1;
 
-//     renderScene(sceneProgram,viewMatrix,projectionMatrix);
-//     /*
-//     gl.useProgram(programInfo.program);
-//     twgl.setUniforms(programInfo, uniforms);
-//     bufferInfoArray.forEach((bufferInfo) => {
-//       twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
-//       twgl.drawBufferInfo(gl, bufferInfo, gl[type.toUpperCase()]);
-//     });
-//     */
-//     gl.useProgram(boxProgram.program);
+    let beanMatrix =  new Float32Array([
+        beanSX, 0.0, 0.0, 0.0,
+        0.0, beanSY, 0.0, 0.0,
+        0.0, 0.0, beanSZ, 0.0,
+        0.0, 0.0, 0.0, 1.0,
+    ]);
+    let scaleMatrix = gl.getUniformLocation(programInfo.program, 'scaleMatrix');
 
-//     // Camera Body
-//     let modelMatrix = getCameraTransformationMatrix(true);
-//     twgl.setBuffersAndAttributes(gl, boxProgramInfo, boxBufferInfo);
-//     uniforms.modelMatrix = m4.multiply(modelMatrix, m4.translation([0, 0, 1]));
-//     (uniforms.color = [1, 1, 1]), twgl.setUniforms(boxProgramInfo, uniforms);
-//     twgl.drawBufferInfo(gl, boxBufferInfo, gl.LINES);
-//     // Camera eye piece
-//     uniforms.modelMatrix = m4.multiply(
-//       m4.multiply(modelMatrix, m4.translation([0, 0, -0.5])),
-//       m4.scaling([0.5, 0.5, 0.5])
-//     );
-//     twgl.setUniforms(boxProgramInfo, uniforms);
-//     twgl.drawBufferInfo(gl, boxBufferInfo, gl.LINES);
+    // Translating beans
+    let beanTX = 0.0, beanTY = 0.0, beanTZ = 0.0
+    let translation = gl.getUniformLocation(programInfo.program, 'translation' );
+    
+    gl.useProgram(programInfo.program);
+    const uniforms = {
+        n2c: 0,
+        modelMatrix: viewMatrix,
 
-//     // Camera Frustum
-//     modelMatrix = getCameraTransformationMatrix(false);
+    };
+    twgl.setUniforms(programInfo, uniforms);
+    let bufferInfoArr = bufferInfoArray(model);
+    bufferInfoArr.forEach((bufferInfo) => {
+        twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+        twgl.drawBufferInfo(gl, bufferInfo, gl["TRIANGLES"]);
+    });
 
-//     uniforms.modelMatrix = m4.multiply(
-//       modelMatrix,
-//       m4.inverse(getProjectionMatrix(fov_Y, near, far))
-//     );
-//     uniforms.color = [0.5, 0.5, 0.5];
-//     twgl.setUniforms(boxProgramInfo, uniforms);
-//     twgl.drawBufferInfo(gl, boxBufferInfo, gl.LINES);
-// }
-
-// function boxBufferInfo(gl) {
-//     const boxAttributes = {
-//       position: [
-//         -1,
-//         -1,
-//         -1,
-
-//         1,
-//         -1,
-//         -1,
-
-//         1,
-//         1,
-//         -1,
-
-//         -1,
-//         1,
-//         -1,
-
-//         -1,
-//         -1,
-//         1,
-
-//         1,
-//         -1,
-//         1,
-
-//         1,
-//         1,
-//         1,
-
-//         -1,
-//         1,
-//         1
-//       ],
-//       indices: [
-//         0,
-//         1,
-//         1,
-//         2,
-//         2,
-//         3,
-//         3,
-//         0,
-//         4,
-//         5,
-//         5,
-//         6,
-//         6,
-//         7,
-//         7,
-//         4,
-//         0,
-//         4,
-//         1,
-//         5,
-//         2,
-//         6,
-//         3,
-//         7
-//       ]
-//     };
-//     return twgl.createBufferInfoFromArrays(gl, boxAttributes);
-// }
+    gl.uniformMatrix4fv(scaleMatrix,false,beanMatrix);
+    gl.uniform3f(translation, beanTX, beanTY, beanTZ);
+}
 
 function bufferInfoArray(model) {
     return model.map((vertexAttributes) =>
         twgl.createBufferInfoFromArrays(gl, vertexAttributes)
     );
+}
+
+function getAngle(x1, y1, x2, y2) {
+    y1 = -y1;
+    y2 = -y2;
+
+    var distY = y2 - y1; //opposite
+    var distX = x2 - x1; //adjacent
+
+    if (distX == 0) distX = 0.01;
+    if (distY == 0) distY = 0.01;
+    let angles = {
+        x: distX / 100,
+        y: distY / 10,
+    };
+    return angles; //return angle in degrees
 }
 
 function sceneProgramInfo(gl) {
@@ -293,25 +284,40 @@ function sceneProgramInfo(gl) {
     });
 }
 
-// function boxProgramInfo(gl) {
-//     const vs = `#version 300 es
-//       precision highp float;
-//       in vec3 position;
+function beanProgramInfo(gl) {
+    const shaders = {
+        vs: `#version 300 es
+      precision mediump float;
+      in vec3 position;
+      in vec3 normal;
     
-//       uniform mat4 modelMatrix;
-//       uniform mat4 viewMatrix;
-//       uniform mat4 projectionMatrix;
-//       void main () {
-//         gl_Position = projectionMatrix*viewMatrix*modelMatrix*vec4(position,1);
-//       }`,
-//         fs = `#version 300 es
-//       precision highp float;
-//       out vec4 outColor;
-//       uniform vec3 color;
-//       void main () {
-//         outColor = vec4(color,1);
-//       }`;
-//     return twgl.createProgramInfo(gl, [vs, fs]);
-// }
+      uniform vec3 translation;
+      uniform mat4 scaleMatrix;
+      uniform mat4 modelMatrix;
+
+      out vec3 fragNormal;
+      void main () {
+        vec4 newPosition = modelMatrix*vec4(position,1.0);
+        gl_Position = modelMatrix*vec4(position+translation,1.0)*scaleMatrix;
+        mat4 normalMatrix = transpose(inverse(modelMatrix));
+        fragNormal = normalize((normalMatrix*vec4(normal,0)).xyz);
+        gl_PointSize = 2.;
+      }`,
+
+        fs: `#version 300 es
+      precision mediump float;
+      out vec4 outColor;
+      in vec3 fragNormal;
+      uniform int n2c;
+      void main () {
+        vec3 N = normalize(fragNormal);
+        vec3 color = (n2c==0)?abs(N):(N+1.)/2.;
+        outColor = vec4(color, 1);
+      }`,
+    };
+    return twgl.createProgramInfo(gl, [shaders.vs, shaders.fs], (message) => {
+        console.log("Program Shader compilation error\n" + message);
+    });
+}
 
 window.onload = main;
